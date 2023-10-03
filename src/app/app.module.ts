08fbde616f13ffeaa9b493d6b2e17e94ef21796c
unknown <antonismisirgis@gmail.com>
2023-10-03 17:30:41 +0300

    added material, bootstrap and some initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CenterComponent } from './components/center/center.component';
+import { ArticleComponent } from './components/article/article.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlaceholderComponent,
+    FooterComponent,
+    CenterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
